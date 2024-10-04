8125a6cb7e0789ba6ea5e185c325f1ca59665937
Max <maxim@injectivelabs.org>
2024-10-04 21:26:18 +0400

    upd: breakdown for msgs, events, unpack authz msgs

--- pkg/coremon/block_handler.go
+++ pkg/coremon/block_handler.go
@@ -2,11 +2,16 @@ package coremon
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 	"time"
 
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	"github.com/cosmos/gogoproto/proto"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	influxdb2api "github.com/influxdata/influxdb-client-go/v2/api"
 	influxwrite "github.com/influxdata/influxdb-client-go/v2/api/write"
+	"github.com/pkg/errors"
 	"github.com/xlab/pace"
 	metrics "github.com/xlab/statsd_metrics"
 	log "github.com/xlab/suplog"
@@ -48,8 +53,11 @@ func NewBlockHandlerWithMetrics(
 	})
 
 	return func(data NewBlockData) error {
+		handlerStart := time.Now()
+
 		blockNumber := uint64(data.Block.Height)
-		txsInBlock := len(data.Block.Txs)
+		txsInBlock := len(data.BlockResults.TxsResults)
+		blockEvents := len(data.BlockResults.FinalizeBlockEvents)
 
 		latency := time.Since(data.Block.Time)
 		logger.WithFields(log.Fields{
@@ -69,8 +77,14 @@ func NewBlockHandlerWithMetrics(
 			// txBytes is the cummulative size of all txns in the block
 			txBytes int
 
-			// txGas is the cummulative gas spent on all txns in the block
-			txGas int64
+			// txGasUsed is the cummulative gas spent on all txns in the block
+			txGasUsed int64
+
+			// txGasWanted is the cummulative gas spent on all txns in the block
+			txGasWanted int64
+
+			// txEventsPerBlock is the number of tx events per block
+			txEventsPerBlock int64
 		)
 
 		blockTimestamps[blockNumber] = data.Block.Time
@@ -85,7 +99,9 @@ func NewBlockHandlerWithMetrics(
 			}
 
 			for _, txResult := range data.BlockResults.TxsResults {
-				txGas += txResult.GasUsed
+				txGasUsed += txResult.GasUsed
+				txGasWanted += txResult.GasWanted
+				txEventsPerBlock += int64(len(txResult.Events))
 			}
 		}
 
@@ -105,8 +121,6 @@ func NewBlockHandlerWithMetrics(
 
 		pointsToWrite := make([]*influxwrite.Point, 0, 1)
 		{
-			// assemble 'coremon_block_report' measurement point with all fields about particular block
-
 			p := influxdb2.NewPointWithMeasurement("coremon_block_report")
 			p = p.SetTime(data.Block.Time)
 			p = p.AddField("height", data.Block.Height)
@@ -115,7 +129,16 @@ func NewBlockHandlerWithMetrics(
 				p = p.AddField("txs", txsInBlock)
 
 				p = p.AddField("txs_bytes", txBytes)
-				p = p.AddField("txs_gas", txGas)
+				p = p.AddField("txs_gas", txGasUsed)
+				p = p.AddField("txs_gas_wanted", txGasWanted)
+
+				if txEventsPerBlock > 0 {
+					p = p.AddField("txs_events", txEventsPerBlock)
+				}
+			}
+
+			if blockEvents > 0 {
+				p = p.AddField("block_events", blockEvents)
 			}
 
 			if blockTimeDiff > 0 {
@@ -135,16 +158,157 @@ func NewBlockHandlerWithMetrics(
 			pointsToWrite = append(pointsToWrite, p)
 		}
 
+		for _, event := range data.BlockResults.FinalizeBlockEvents {
+			p := influxdb2.NewPointWithMeasurement("coremon_block_events")
+			p = p.SetTime(data.Block.Time)
+			p = p.AddField("height", data.Block.Height)
+			p = p.AddTag("ev_type", event.Type)
+
+			allTags := metricTags.WithBaseTags()
+			allTags.Range(func(k, v string) bool {
+				p = p.AddTag(k, v)
+				return false
+			})
+
+			pointsToWrite = append(pointsToWrite, p)
+
+			for _, attr := range event.Attributes {
+				p := influxdb2.NewPointWithMeasurement("coremon_block_events_attrs")
+				p = p.SetTime(data.Block.Time)
+				p = p.AddField("height", data.Block.Height)
+				p = p.AddTag("ev_type", event.Type)
+				p = p.AddTag("attr_key", attr.Key)
+				p = p.AddField("valsize", len(attr.Value))
+				if val, err := strconv.Atoi(attr.Value); err == nil {
+					p = p.AddField("val", int64(val))
+				}
+
+				allTags := metricTags.WithBaseTags()
+				allTags.Range(func(k, v string) bool {
+					p = p.AddTag(k, v)
+					return false
+				})
+
+				pointsToWrite = append(pointsToWrite, p)
+			}
+		}
+
+		for txIndex, txResult := range data.BlockResults.TxsResults {
+			for _, event := range txResult.Events {
+				p := influxdb2.NewPointWithMeasurement("coremon_tx_events")
+				p = p.SetTime(data.Block.Time)
+				p = p.AddField("height", data.Block.Height)
+				p = p.AddTag("ev_type", event.Type)
+
+				allTags := metricTags.WithBaseTags()
+				allTags.Range(func(k, v string) bool {
+					p = p.AddTag(k, v)
+					return false
+				})
+
+				pointsToWrite = append(pointsToWrite, p)
+
+				for _, attr := range event.Attributes {
+					p := influxdb2.NewPointWithMeasurement("coremon_tx_events_attrs")
+					p = p.SetTime(data.Block.Time)
+					p = p.AddField("height", data.Block.Height)
+					p = p.AddTag("ev_type", event.Type)
+					p = p.AddTag("attr_key", attr.Key)
+					p = p.AddField("valsize", len(attr.Value))
+					if val, err := strconv.Atoi(attr.Value); err == nil {
+						p = p.AddField("val", int64(val))
+					}
+					p = p.AddField("gas_wanted", txResult.GasWanted)
+					p = p.AddField("gas_used", txResult.GasUsed)
+					p = p.AddField("events", len(txResult.Events))
+
+					allTags := metricTags.WithBaseTags()
+					allTags.Range(func(k, v string) bool {
+						p = p.AddTag(k, v)
+						return false
+					})
+
+					pointsToWrite = append(pointsToWrite, p)
+				}
+			}
+
+			p := influxdb2.NewPointWithMeasurement("coremon_txs")
+			p = p.SetTime(data.Block.Time)
+			p = p.AddField("height", data.Block.Height)
+			p = p.AddField("tx_idx", txIndex)
+			p = p.AddField("events", len(txResult.Events))
+			p = p.AddField("datasize", len(txResult.Data))
+			p = p.AddTag("code", fmt.Sprintf("%d", txResult.Code))
+			p = p.AddTag("codespace", txResult.Codespace)
+
+			parsedTx, err := decodeABCITx(data.Block.Txs[txIndex])
+			if err != nil {
+				err = errors.Wrap(err, "failed to decode ABCI Tx")
+				return err
+			}
+
+			msgs := parsedTx.GetMsgs()
+
+			filteredMsgs := make([]proto.Message, 0, len(msgs))
+			for _, msg := range msgs {
+				if msgExec, ok := msg.(*authz.MsgExec); ok {
+					for _, authzInternalAny := range msgExec.Msgs {
+						// append interal authz msgs only
+						var authzInternalMsg proto.Message
+						if err := injectiveCdc.UnpackAny(authzInternalAny, &authzInternalMsg); err != nil {
+							err = errors.Wrapf(err, "failed to unpack any from %s", authzInternalAny.TypeUrl)
+							return err
+						}
+
+						filteredMsgs = append(filteredMsgs, authzInternalMsg)
+					}
+				} else {
+					// not authz, append as-is
+					filteredMsgs = append(filteredMsgs, msg)
+				}
+			}
+
+			p = p.AddField("msgs", len(filteredMsgs))
+
+			allTags := metricTags.WithBaseTags()
+			allTags.Range(func(k, v string) bool {
+				p = p.AddTag(k, v)
+				return false
+			})
+
+			pointsToWrite = append(pointsToWrite, p)
+
+			for _, msg := range filteredMsgs {
+				p := influxdb2.NewPointWithMeasurement("coremon_tx_msgs")
+				p = p.SetTime(data.Block.Time)
+				p = p.AddField("height", data.Block.Height)
+				p = p.AddField("tx_idx", txIndex)
+				p = p.AddTag("msg_name", proto.MessageName(msg))
+
+				allTags := metricTags.WithBaseTags()
+				allTags.Range(func(k, v string) bool {
+					p = p.AddTag(k, v)
+					return false
+				})
+
+				pointsToWrite = append(pointsToWrite, p)
+			}
+		}
+
 		if len(pointsToWrite) > 0 {
 			ctx, cancelFn := context.WithTimeout(
 				context.Background(),
-				10*time.Second,
+				1*time.Minute,
 			)
 			writeInfluxPoints(ctx, logger, influxWriteAPI, pointsToWrite)
 			cancelFn()
 
 			influxPointsOutPace.StepN(len(pointsToWrite))
 		}
+
+		metrics.CustomReport(func(s metrics.Statter, tagSpec []string) {
+			s.Timing("report.block_handler_dur", time.Since(handlerStart), tagSpec)
+		}, metricTags)
 
 		return nil
 	}
